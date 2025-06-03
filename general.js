26b341b0dd3cad5a536ced89a95089b65b759092
AnUnknownC <88901481+AnUnknownC@users.noreply.github.com>
2025-06-02 23:40:16 -0500

    Completed the information and fix proportion in each html

--- general.js
+++ general.js
@@ -4,11 +4,47 @@ const sidebar = document.getElementById('sidebar');
 toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('show');
 });
+// Animacion carrusel
 
-// Cierra el menú automáticamente al hacer clic en un enlace (opcional)
-const links = sidebar.querySelectorAll('a');
-links.forEach(link => {
-    link.addEventListener('click', () => {
-        sidebar.style.left = '-250px';
-    });
-});
+let imagenes = [
+  {"url" : "img/Evento/IMG-20250601-WA0012.jpg"}, 
+  {"url" : "img/Evento/IMG-20250601-WA0023.jpg"},
+  {"url" : "img/Evento/IMG-20250601-WA0028.jpg"}
+]
+
+let atras = document.getElementById('atras');
+let adelante = document.getElementById('adelante');
+let imagen = document.getElementById('img');
+let puntos = document.getElementById('puntos');
+let texto = document.getElementById('texto')
+let actual = 0
+posicionCarrusel()
+
+atras.addEventListener('click', function(){
+  actual -=1
+  if(actual == -1){
+    actual = imagenes.length - 1
+  }
+  imagen.innerHTML = ` <img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`
+  posicionCarrusel()
+})
+
+adelante.addEventListener('click', function(){
+  actual +=1
+  if(actual == imagenes.length){
+    actual = 0
+  }
+  imagen.innerHTML = ` <img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`
+  posicionCarrusel()
+})
+
+function posicionCarrusel(){
+  puntos.innerHTML = ""
+  for(var i = 0; i < imagenes.length; i++){
+    if(i == actual){
+      puntos.innerHTML += '<p class = "bold">.<p>'
+    } else{
+      puntos.innerHTML += '<p>.<p>'
+    }
+  }
+}
